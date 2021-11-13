229586320facb3cf41af76c7ff8a9afa4c2dcc31
hou.y.h <405469210@qq.com>
2021-11-13 23:33:22 +0800

    feat: vue3 + typescript 的项目基本配置和框架

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>hello</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data () {
+        return {};
+    }
+})
+</script>
+
+<style lang="css">
+  	
+</style>
